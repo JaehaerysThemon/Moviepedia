d7b5221020be06b1542d73b070267dc42cdfdf51
JaehaerysT <54642148+JaehaerysThemon@users.noreply.github.com>
2020-09-16 15:17:50 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,36 @@
+<?php
+  include('../include/session.inc.php');
+  logIn("Leon", true);
+?>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark p-3">
+  <a class="navbar-brand" href="../sites/home.php">Moviepedia</a>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <?php
+      if(isLoggedIn()){
+        $tabs='<li class="nav-item active">
+          <a class="nav-link" href="../sites/create.php">Create Page<span class="sr-only">(current)</span></a>
+          </li>';
+        if(isMod()){
+          $tabs.='<li class="nav-item active">
+            <a class="nav-link" href="../sites/sign.php">Sign Pages<span class="sr-only">(current)</span></a>
+            </li>';
+        }
+        $tabs.='<li class="nav-item active">
+          <a class="nav-link" href="../sites/logout.php">Account<span class="sr-only">(current)</span></a>
+          </li>';
+        echo $tabs;
+      } else {
+        echo('<li class="nav-item active">
+        <a class="nav-link" href="../sites/login.php">Log In <span class="sr-only">(current)</span></a>
+        </li>');
+      }
+
+      ?>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
