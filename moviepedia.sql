629614e44db4df738935e57fc07a85a39055b3fc
Jaehaerys Themon <54642148+JaehaerysThemon@users.noreply.github.com>
2020-10-19 22:42:45 +0200

    Update moviepedia.sql

--- moviepedia.sql
+++ moviepedia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 16, 2020 at 12:07 PM
+-- Generation Time: Oct 19, 2020 at 10:40 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.9
 
@@ -36,6 +36,13 @@ CREATE TABLE `pages` (
   `author` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `pages`
+--
+
+INSERT INTO `pages` (`id`, `title`, `text`, `approved`, `parent`, `author`) VALUES
+(2, 'Hello', 'gsQDDFGHJKLÖÖÖÖÖÖÖLLLLLLkllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll', 0, NULL, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -51,6 +58,21 @@ CREATE TABLE `users` (
   `password` varchar(100) NOT NULL,
   `moderator` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `username`, `email`, `password`, `moderator`) VALUES
+(1, 'HJGDUZJGsadas', 'Leoekto', 'Leon', 'leonkleiber3@gmail.com', '$2y$10$WtgxB7UwVSdvhW/zES4N9OnbyEs.lL85gA4X.1zy7o4fkuGqc75t.', 0),
+(2, 'Leon', 'Kleiber', 'JaehaerysT', 'leon.kleiber@gibmit.ch', '$2y$10$WtgxB7UwVSdvhW/zES4N9OnbyEs.lL85gA4X.1zy7o4fkuGqc75t.', 1),
+(3, 'KLWJqdd', 'ILjDKLDJDNDKdjkshfj', 'Leon', 'dkfdfj22@kdjfkl.dfkl', '$2y$10$gWlvgPHLBhpCfsa0lkQP5.esKkz7ih0.DA1f700R8QSnjRZL.ZX46', 0),
+(4, 'KLWJqdd', 'ILjDKLDJDNDKdjkshfj', 'Leon', 'dkfdfj22@kdjfkl.dfkl', '$2y$10$k8h//BPJ5OZPIyhWVo2FfOBG8pH7TTBzx35O9U.lug2fdIuch6HNa', 0),
+(5, 'KLWJqdd', 'ILjDKLDJDNDKdjkshfj', 'Leon', 'dkfdfj22@kdjfkl.dfkl', '$2y$10$gwrrehceN8N./wqZivj0leiQP7glUlxPe/9MsVnxb/oPgzOKwyzH6', 0),
+(6, 'Leon', 'Kleiber', 'JaehaerysT', 'leonkleiber3@gmail.com', '$2y$10$AgI0kxgFGXQGiPgQ.J90gekRNS1tXJ.ecktBuEc8rbm47HAzJOjni', 0),
+(7, 'Leon', 'Kleiber', 'ilgjfioldhfigH', 'leonkleiber3@gmail.com', '$2y$10$IXAPQ38LAgNUPjy.AROWU.LYWARSZuuX07KQ0dCPqannJAB83BWzS', 0),
+(8, 'Leon', 'Kleiber', 'ilgjfioldhfigH', 'leonkleiber3@gmail.com', '$2y$10$SDzwKFcZ.hfQbrH.NLyePewPkfeCIVPRZlVYMBZVr2azSSRBVuLWC', 1),
+(9, 'Kkjhfdsew', 'Kleiber', 'Fisc', 'leonkleiber3@gmail.com', '$2y$10$j.DFR/tY9BTSa/h4EFD4FOGhnb.XrA2eQDLqsmgNTruHDk5.u8irC', 1);
 
 --
 -- Indexes for dumped tables
@@ -78,13 +100,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `pages`
 --
 ALTER TABLE `pages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Constraints for dumped tables
